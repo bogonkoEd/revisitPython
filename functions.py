72e0fd5ad98dace50a22ba8e9d44517d77f47057
Glen-2339 <glen.lihanda@strathmore.edu>
2023-10-02 18:34:50 +0300

    I made some changes to the code allowing it to sava data from excel files as jsonl

--- functions.py
+++ functions.py
@@ -20,3 +20,19 @@ def load_dataset(dataset_path):
     data_frame = pandas.DataFrame(data)
 
     return data_frame
+    
+    
+def filter_into_jsonl(xlsx_file_path, output_dir, filter_column, filter_value):
+    """Reads data from Excel file, filters it based on a specified column and value and saves as jsonl file
+    Args:
+        xlsx_file_path (str): Input Excel file path, output_dir (str): Output directory for JSON Lines file, filter_column (str): Column to filter
+        filter_value (str): Value to filter by"""
+
+    data_f = pd.read_excel(xlsx_file_path)
+    filtered_data = data_f[data_f[filter_column] == filter_value]
+    filtered_data_dict = filtered_data.to_dict(orient='records')
+
+    jsonl_file_path = os.path.join(output_dir, f'{filter_value}.jsonl')
+    with open(jsonl_file_path, 'w', encoding='utf-8') as jsonl_file:
+        for record in filtered_data_dict:
+            jsonl_file.write(json.dumps(record, ensure_ascii=False) + '\n')
