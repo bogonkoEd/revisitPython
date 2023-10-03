36d370428c1a0ad0ed2bb4adc2465913b6301648
Martin Mwangi <martin.wangombe@strathmore.edu>
2023-10-03 13:51:39 +0300

    Translation Function

--- functions.py
+++ functions.py
@@ -20,3 +20,30 @@ def load_dataset(dataset_path):
     data_frame = pandas.DataFrame(data)
 
     return data_frame
+
+def generate_translation_jsonl(dataframe, output_dir):
+    """ Combines translations from a DataFrame and saves them as (.jsonl) file.
+         Args:
+        dataframe (pandas.DataFrame): DataFrame containing translation data. output_dir (str): Directory to save the combined file."""
+
+    locales = dataframe['locale'].unique()
+
+    combined_data_list = []
+
+    for locale in locales:
+
+        if locale == 'en-US':
+
+            continue
+
+        locale_data = dataframe[dataframe['locale'] == locale]
+        combined_data = pandas.merge(en_data, locale_data, on='id', how='inner')
+        combined_data_list.extend(combined_data.to_dict('records'))
+
+    jsonl_file_path = os.path.join(output_dir, 'combined_translation.jsonl')
+
+    with open(jsonl_file_path, 'w', encoding='utf-8') as jsonl_file:
+
+        for record in combined_data_list:
+            
+            jsonl_file.write(json.dumps(record, ensure_ascii=False) + '\n')
