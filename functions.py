b69fd63799c6564eed54cb075b1bc2c4cdb778c3
Eddy Bogonko <eddy.bogonko@starthmore.edu>
2023-10-03 18:23:37 +0300

    Fix merging deletion error

--- functions.py
+++ functions.py
@@ -13,10 +13,11 @@ def load_dataset(dataset_path):
     with tarfile.open(dataset_path, mode="r:gz") as data_archive:
         for member in data_archive.getmembers():
             if member.isfile() and member.name.endswith(".jsonl"):
-                jsonl_data = data_archive.extractfile(member).read().decode("utf-8")
-                for line in jsonl_data.split("\n"):
-                    if line:
-                        data.append(json.loads(line))
+                with data_archive.extractfile(member) as jsonl_data:
+                    for line_bytes in jsonl_data:
+                        line = line_bytes.decode("utf-8").strip()
+                        if line:
+                            data.append(json.loads(line))
 
     data_frame = pandas.DataFrame(data)
 
@@ -24,59 +25,57 @@ def load_dataset(dataset_path):
 
 
 def generate_translation_jsonl(dataframe, output_dir):
-    """ Combines translations from a DataFrame and saves them as (.jsonl) file.
-         Args:
-        dataframe (pandas.DataFrame): DataFrame containing translation data. output_dir (str): Directory to save the combined file."""
+    """Combines translations from a DataFrame and saves them as (.jsonl) file.
+     Args:
+    dataframe (pandas.DataFrame): DataFrame containing translation data. output_dir (str): Directory to save the combined file.
+    """
 
-    locales = dataframe['locale'].unique()
+    locales = dataframe["locale"].unique()
 
     combined_data_list = []
 
-
     for locale in locales:
-
-        if locale == 'en-US':
-
+        if locale == "en-US":
             continue
+        en_data = dataframe[dataframe["locale"] == "en-US"][["id", "utt", "annot_utt"]]
+        locale_data = dataframe[dataframe["locale"] == locale]
+        combined_data = pandas.merge(en_data, locale_data, on="id", how="inner")
+        combined_data_list.extend(combined_data.to_dict("records"))
 
-        locale_data = dataframe[dataframe['locale'] == locale]
-        combined_data = pandas.merge(en_data, locale_data, on='id', how='inner')
-        combined_data_list.extend(combined_data.to_dict('records'))
+    jsonl_file_path = os.path.join(output_dir, "combined_translation.jsonl")
 
-    jsonl_file_path = os.path.join(output_dir, 'combined_translation.jsonl')
-
-    with open(jsonl_file_path, 'w', encoding='utf-8') as jsonl_file:
-
+    with open(jsonl_file_path, "w", encoding="utf-8") as jsonl_file:
         for record in combined_data_list:
-          
-          jsonl_file.write(json.dumps(record, ensure_ascii=False) + '\n')
+            jsonl_file.write(json.dumps(record, ensure_ascii=False) + "\n")
 
-    
 
 def generate_language_excel_files(data_frame, output_dir):
-
-"""Combines data from 'en-US' and other locales and saves as Excel files.
+    """Combines data from 'en-US' and other locales and saves as Excel files.
     Args:
         data_frame (pd.DataFrame): DataFrame containing data for various locales.
         output_dir (str): Directory to save the generated Excel files."""
-        
-    locales = data_frame['locale'].unique()
 
-    if 'en-US' in locales:
+    locales = data_frame["locale"].unique()
 
-        en_data = data_frame[data_frame['locale'] == 'en-US']
-        output_file_path = os.path.join(output_dir, 'en-en.xlsx')
+    if "en-US" in locales:
+        en_data = data_frame[data_frame["locale"] == "en-US"]
+        output_file_path = os.path.join(output_dir, "en-en.xlsx")
         en_data.to_excel(output_file_path, index=False)
 
-        
-        locale_data = data_frame[data_frame['locale'] == locale]
-        en_us_data = data_frame[data_frame['locale'] == 'en-US'][['id', 'utt', 'annot_utt']]
-        combined_data = pandas.merge(en_us_data, locale_data, on='id', how='inner')
+        for locale in locales:
+            if locale == "en-US":
+                continue
 
-        output_file_path = os.path.join(output_dir, f'en-{locale}.xlsx')
-        combined_data.to_excel(output_file_path, index=False)
+            locale_data = data_frame[data_frame["locale"] == locale]
+            en_us_data = data_frame[data_frame["locale"] == "en-US"][
+                ["id", "utt", "annot_utt"]
+            ]
+            combined_data = pandas.merge(en_us_data, locale_data, on="id", how="inner")
 
-    
+            output_file_path = os.path.join(output_dir, f"en-{locale}.xlsx")
+            combined_data.to_excel(output_file_path, index=False)
+
+
 def filter_into_jsonl(xlsx_file_path, output_dir, filter_column, filter_value):
     """Reads data from Excel file, filters it based on a specified column and value and saves as jsonl file
     Args:
@@ -85,10 +84,9 @@ def filter_into_jsonl(xlsx_file_path, output_dir, filter_column, filter_value):
 
     data_f = pandas.read_excel(xlsx_file_path)
     filtered_data = data_f[data_f[filter_column] == filter_value]
-    filtered_data_dict = filtered_data.to_dict(orient='records')
+    filtered_data_dict = filtered_data.to_dict(orient="records")
 
-    jsonl_file_path = os.path.join(output_dir, f'{filter_value}.jsonl')
-    with open(jsonl_file_path, 'w', encoding='utf-8') as jsonl_file:
+    jsonl_file_path = os.path.join(output_dir, f"{filter_value}.jsonl")
+    with open(jsonl_file_path, "w", encoding="utf-8") as jsonl_file:
         for record in filtered_data_dict:
-          jsonl_file.write(json.dumps(record, ensure_ascii=False) + '\n')
-      
+            jsonl_file.write(json.dumps(record, ensure_ascii=False) + "\n")
