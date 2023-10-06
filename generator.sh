d86ec583a4e5bef520d985c61b26d2181273ef33
Eddy Bogonko <eddy.bogonko@strathmore.edu>
2023-10-06 04:05:28 +0300

    Logging results of generated files

--- generator.sh
+++ generator.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-# Run the Python script with the necessary flags
-python main.py \
+# Define log file names
+
+generator_log="logs/generator.log"
+
+# Run the Python script with the necessary flags and capture the output
+output=$(python main.py \
     --archive_path "data/dataset.tar.gz" \
     --output_directory "language_specific_files" \
     --output_dir "jsonl_files" \
-    --filter_column "partition"  \
+    --filter_column "partition")
+
+# Append the output to the log file
+echo "$output" >> "$generator_log"
+
+# Count the number of generated files and save it to the generator_log
+language_specific_files_count=$(ls -1 "language_specific_files" | wc -l)
+jsonl_files_count=$(ls -1 "jsonl_files" | wc -l)
+echo "Number of files generated in 'language_specific_files': $language_specific_files_count" >> "$generator_log"
+echo "Name of generated files in 'language_specific_files': $(ls -1 "language_specific_files")" >> "$generator_log"
+echo "Number of files generated in 'jsonl_files': $jsonl_files_count" >> "$generator_log"
+echo "Name of generated files in 'jsonl_files': $(ls -1 "jsonl_files")" >> "$generator_log"
--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def main(_):
     os.makedirs(FLAGS.output_dir, exist_ok=True)
     filters = [
         ("language_specific_files/en-en.xlsx", "test"),
-        ("language_specific_files/en-de.xlsx", "train"),
-        ("language_specific_files/en-sw.xlsx", "dev"),
+        ("language_specific_files/en-de.xlsx", "dev"),
+        ("language_specific_files/en-sw.xlsx", "train"),
     ]
     for path, filter_value in filters:
         functions.filter_into_jsonl(
